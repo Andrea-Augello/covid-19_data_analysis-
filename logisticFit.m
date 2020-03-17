c0c272937fa4030416ebfc4c81f20a2b93141171
Andrea-Augello <andrea.augello01@community.unipa.it>
2020-03-17 15:30:10 +0100

    Changed interpolation parameters to achieve better fit for large population values

--- logisticFit.m
+++ logisticFit.m
@@ -6,6 +6,9 @@ function [fitresult, gof] = logisticFit(x, y)
 ft = fittype( '1/(A+B*exp(-C*x))', 'independent', 'x', 'dependent', 'y' );
 opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 opts.Display = 'Off';
+opts.Algorithm = 'Levenberg-Marquardt';
+opts.Robust = 'Bisquare';
+
 opts.StartPoint = [0.913375856139019 0.63235924622541 0.0975404049994095];
 
 % Fit model to data.

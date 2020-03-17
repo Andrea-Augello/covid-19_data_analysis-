d902a6cf1109d56e4240eff213a507d46a492209
Andrea-Augello <andrea.augello01@community.unipa.it>
2020-03-17 10:08:57 +0100

    Uploaded files

---
+++ logisticFit.m
@@ -0,0 +1,13 @@
+function [fitresult, gof] = logisticFit(x, y)
+%% Fit: 'logisticFit'.
+[xData, yData] = prepareCurveData( x, y );
+
+% Set up fittype and options.
+ft = fittype( '1/(A+B*exp(-C*x))', 'independent', 'x', 'dependent', 'y' );
+opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+opts.Display = 'Off';
+opts.StartPoint = [0.913375856139019 0.63235924622541 0.0975404049994095];
+
+% Fit model to data.
+[fitresult, gof] = fit( xData, yData, ft, opts );
+end
